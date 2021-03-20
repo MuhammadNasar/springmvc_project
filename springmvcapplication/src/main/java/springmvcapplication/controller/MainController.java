b55509e5e023997e6df818f473f3836643f712a5
Muhammad Nasar <muhammadnasar008@gmail.com>
2021-03-21 02:57:21 +0500

    in views index.jsp file is added, and configuration of spring ORM is done in spring-servlet.xml file

--- springmvcapplication/src/main/java/springmvcapplication/controller/MainController.java
+++ springmvcapplication/src/main/java/springmvcapplication/controller/MainController.java
@@ -1,5 +1,14 @@
 package springmvcapplication.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class MainController {
 
+	@RequestMapping("/new")
+	public String home() {
+		
+		return "index";
+	}
 }
